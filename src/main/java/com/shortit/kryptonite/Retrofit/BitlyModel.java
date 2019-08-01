a8152f8cae60f947b0c0b9f93b9a964552c1e724
mGeri <gerasz01@gmail.com>
2019-08-01 20:45:58 +0200

    Not Lombok constructors

--- src/main/java/com/shortit/kryptonite/Retrofit/BitlyModel.java
+++ src/main/java/com/shortit/kryptonite/Retrofit/BitlyModel.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class BitlyModel {
@@ -23,4 +21,24 @@ public class BitlyModel {
   private String client_id;
   private String[] custom_bitlinks;
   private String id;
+
+  public BitlyModel(String long_url, String link, Object references, boolean archived,
+      String[] tags, String created_at, String title, Object[] deeplinks, String created_by,
+      String client_id, String[] custom_bitlinks, String id) {
+    this.long_url = long_url;
+    this.link = link;
+    this.references = references;
+    this.archived = archived;
+    this.tags = tags;
+    this.created_at = created_at;
+    this.title = title;
+    this.deeplinks = deeplinks;
+    this.created_by = created_by;
+    this.client_id = client_id;
+    this.custom_bitlinks = custom_bitlinks;
+    this.id = id;
+  }
+
+  public BitlyModel() {
+  }
 }
