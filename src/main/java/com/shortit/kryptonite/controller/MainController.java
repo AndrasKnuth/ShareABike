ffac87a658d7b1b26d913e048eaa7e6e2c2d5ad0
Andras Knuth <knuth.andras89@gmail.com>
2019-08-01 11:30:02 +0200

    create project

---
+++ src/main/java/com/shortit/kryptonite/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.shortit.kryptonite.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  @PostMapping("http://localhost:8080/shortit")
+  public String shortit(HttpServletRequest httpServletRequest) {
+    String kutya = "kutya";
+    System.out.println(httpServletRequest.toString());
+    return kutya;
+  }
+}
