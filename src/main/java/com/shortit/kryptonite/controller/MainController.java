c766b9d2607b57067d4645a8b82668f5946871bd
Andras Knuth <knuth.andras89@gmail.com>
2019-08-01 16:58:38 +0200

    test

--- src/main/java/com/shortit/kryptonite/controller/MainController.java
+++ src/main/java/com/shortit/kryptonite/controller/MainController.java
@@ -3,13 +3,29 @@ package com.shortit.kryptonite.controller;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import model.SlashText;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-  @PostMapping("/shortit")
+
+  @RequestMapping(
+      value = "/shortit", method = RequestMethod.POST,
+      consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public @ResponseBody
+  String slack(@RequestBody SlashText slackSlashCommand) {
+    return slackSlashCommand.getText();
+  }
+
+  /*@PostMapping("/shortit")
   public String shortit(HttpServletRequest httpServletRequest) {
     List<String> strings = new ArrayList<>();
     String kutya = "kutya";
@@ -24,5 +40,5 @@ public class MainController {
     strings.add(httpServletRequest.getHeader("text"));
     strings.add(httpServletRequest.getParameter("text"));
     return kutya;
-  }
+  }*/
 }
