2f74af2b3aa1e96e55f1fb8cae9ecd56d2d647b6
Andras Knuth <knuth.andras89@gmail.com>
2019-08-01 16:21:52 +0200

    feat(project): fix Dé's problem

--- src/main/java/com/shortit/kryptonite/controller/MainController.java
+++ src/main/java/com/shortit/kryptonite/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.shortit.kryptonite.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,17 @@ public class MainController {
 
   @PostMapping("/shortit")
   public String shortit(HttpServletRequest httpServletRequest) {
+    List<String> strings = new ArrayList<>();
     String kutya = "kutya";
-    System.out.println(httpServletRequest.toString());
+
+
     System.out.println(httpServletRequest.getHeaderNames());
-    System.out.println(httpServletRequest.getMethod());
-    System.out.println(httpServletRequest.getQueryString());
+
     //System.out.println(httpServletRequest.getInputStream().toString());
     System.out.println(httpServletRequest.getParameterMap().entrySet());
-    return kutya;
+    strings.add(httpServletRequest.getQueryString());
+    strings.add(httpServletRequest.getHeader("text"));
+    strings.add(httpServletRequest.getParameter("text"));
+    return httpServletRequest.getHeader("text");
   }
 }
