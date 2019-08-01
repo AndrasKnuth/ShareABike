f0b326babb394aaf2199cfd2606b250487011a55
Daniel Fodor <daniel.fodor91@gmail.com>
2019-08-01 15:59:42 +0200

    retrofit implemented

--- src/main/java/com/shortit/kryptonite/Retrofit/BitlyClient.java
+++ src/main/java/com/shortit/kryptonite/Retrofit/BitlyClient.java
@@ -1,7 +1,11 @@
 package com.shortit.kryptonite.Retrofit;
 
+import java.io.IOException;
 import org.springframework.stereotype.Component;
+import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Component
 public class BitlyClient {
@@ -9,7 +13,26 @@ public class BitlyClient {
   private BitlyInterface bitlyInterface;
 
   public BitlyClient() {
-    Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl()
+    Retrofit retrofit =
+        new Retrofit.Builder()
+            .baseUrl("https://api-ssl.bitly.com/v4")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    this.bitlyInterface = retrofit.create(BitlyInterface.class);
+  }
+
+  public BitlyModel getShortUrl() throws IOException {
+
+    String token = "DSADSADASDASDSDADDSD";
+    Call<BitlyModel> callSync = bitlyInterface.shortenUrl(token);
+
+    Response<BitlyModel> response = callSync.execute();
+
+    if (!response.isSuccessful()) {
+      throw new IOException(
+          response.errorBody() != null ? response.errorBody().string() : "Unknown Error");
+    }
+    return response.body();
   }
 }
--- src/main/java/com/shortit/kryptonite/Retrofit/BitlyInterface.java
+++ src/main/java/com/shortit/kryptonite/Retrofit/BitlyInterface.java
@@ -1,9 +1,10 @@
 package com.shortit.kryptonite.Retrofit;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
 
 public interface BitlyInterface {
-  @GET("/shorten")
-  Call<BitlyModel> shortenUrl();
+  @POST("/shorten")
+  Call<BitlyModel> shortenUrl(@Header("authorization") String token);
 }
